251de1f1d05d57771605994358e520ea99f9f24a
Juanchi Parra <juanchiparra@gmail.com>
2025-02-07 23:00:11 -0400

    Create challenge01.js

---
+++ 2025/Challenge01/challenge01.js
@@ -0,0 +1,8 @@
+export const data = [
+	{ year: 1875, value: 1263902 },
+	{ year: 1880, value: 1522173 },
+	{ year: 1885, value: 2362889 },
+	{ year: 1890, value: 3485176 },
+	{ year: 1895, value: 4158960 },
+	{ year: 1899, value: 4220120 }
+];
