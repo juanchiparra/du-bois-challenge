5a8b481644ed8138dbc15bd97cdc95081818b9df
Juanchi Parra <juanchiparra@gmail.com>
2025-02-11 12:18:54 -0400

    Create challenge02.js

---
+++ 2025/Challenge02/challenge02.js
@@ -0,0 +1,28 @@
+export const data = [
+	{ year: 1874, value: 338769.0 },
+	{ year: 1875, value: 395669.9 },
+	{ year: 1876, value: 460699.4 },
+	{ year: 1877, value: 468828.1 },
+	{ year: 1878, value: 509471.6 },
+	{ year: 1879, value: 550115.1 },
+	{ year: 1880, value: 598887.3 },
+	{ year: 1882, value: 696431.7 },
+	{ year: 1883, value: 672045.6 },
+	{ year: 1884, value: 769589.9 },
+	{ year: 1885, value: 785847.3 },
+	{ year: 1886, value: 810233.4 },
+	{ year: 1887, value: 818362.1 },
+	{ year: 1888, value: 875263.0 },
+	{ year: 1889, value: 891520.4 },
+	{ year: 1890, value: 964678.6 },
+	{ year: 1891, value: 1013450.8 },
+	{ year: 1892, value: 1070351.7 },
+	{ year: 1893, value: 1045965.6 },
+	{ year: 1894, value: 1070351.7 },
+	{ year: 1895, value: 1037836.9 },
+	{ year: 1896, value: 1037836.9 },
+	{ year: 1897, value: 1062223.0 },
+	{ year: 1898, value: 1102866.5 },
+	{ year: 1899, value: 1062223.0 }
+];
+
