f8b7287a009f2596aff2936e108a2d264ade682e
Juanchi Parra <juanchiparra@gmail.com>
2025-03-13 10:38:46 -0400

    Create challenge06.js

---
+++ 2025/Challenge06/challenge06.js
@@ -0,0 +1,8 @@
+export const data = [
+    { city: "Savannah", year: 1880, owners: 699, value: 194392, showLabel: false, labelX: 0, labelY: 0 },
+    { city: "Savannah", year: 1890, owners: 1387, value: 550145, showLabel: false, labelX: 0, labelY: 0 },
+    { city: "Savannah", year: 1898, owners: 2157, value: 872737, showLabel: true, labelX: 570, labelY: 125 },
+    { city: "Atlanta", year: 1880, owners: 639, value: 227955, showLabel: true, labelX: 96, labelY: 560 },
+    { city: "Atlanta", year: 1890, owners: 1120, value: 768256, showLabel: false, labelX: 0, labelY: 0 },
+    { city: "Atlanta", year: 1898, owners: 1112, value: 1308995, showLabel: true, labelX: 566, labelY: 447 },
+];
