462b01994c78dbf259ef48039dfe2f497ddc94b2
Juanchi Parra <juanchiparra@gmail.com>
2025-04-03 18:07:30 -0400

    Create challenge09.js

---
+++ 2025/Challenge09/challenge09.js
@@ -0,0 +1,12 @@
+export const data = [
+    { market: "Negroes", segment: "Agriculture, Fisheries and Mining", "value": 62 },
+    { market: "Negroes", segment: "Domestic and Personal\nService", "value": 28 },
+    { market: "Negroes", segment: "Manufacturing and Mechanical\nIndustries", "value": 5 },
+    { market: "Negroes", segment: "Trade and\nTransportation", "value": 4.5 },
+    { market: "Negroes", segment: "Professions", "value": 2 },
+    { market: "Whites", segment: "Agriculture, Fisheries and Mining", "value": 64 },
+    { market: "Whites", segment: "Manufacturing and\nMechanical Industries", "value": 13.5 },
+    { market: "Whites", segment: "Trade and\nTransportation", "value": 13 },
+    { market: "Whites", segment: "Domestic\nand\nPersonal\nService", "value": 5.5 },
+    { market: "Whites", segment: "Professions", "value": 4 },
+]
