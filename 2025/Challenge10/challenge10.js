a02282b9c0932d5172dfff16fc1c0f92e607b748
Juanchi Parra <juanchiparra@gmail.com>
2025-04-11 16:21:27 -0400

    Create challenge10.js

---
+++ 2025/Challenge10/challenge10.js
@@ -0,0 +1,9 @@
+export const data = [
+    { class: "$100-200", actualAverage: 139.10, rent: 19, food: 43, clothes: 28, tax: 9.9, other: 0 },
+    { class: "$200-300", actualAverage: 249.45, rent: 22, food: 47, clothes: 23, tax: 4, other: 4 },
+    { class: "$300-400", actualAverage: 335.66, rent: 23, food: 43, clothes: 18, tax: 4.5, other: 11.5 },
+    { class: "$400-500", actualAverage: 433.82, rent: 18, food: 37, clothes: 15, tax: 5.5, other: 24.5 },
+    { class: "$500-750", actualAverage: 547.00, rent: 13, food: 31, clothes: 17, tax: 5, other: 34 },
+    { class: "$750-1000", actualAverage: 880.00, rent: 0, food: 37, clothes: 19, tax: 8, other: 36 },
+    { class: "Over $1000", actualAverage: 1125.00, rent: 0, food: 29, clothes: 16, tax: 4.5, other: 50.5 },
+];
